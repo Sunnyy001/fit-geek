30c80ae4da2ce83d50e166c51b0b338736202acc
Akshay Kalushe <akshaykalushe35@gmail.com>
2023-12-20 20:34:52 +0530

    just created the styling for the first container

--- src/landing_page/Land_page.js
+++ src/landing_page/Land_page.js
@@ -1,33 +1,70 @@
 // landing_page/Land_page.js
-
 import React from 'react';
 import './Land_page.css'
+import myImage from '/home/akshay/fit-geek/src/images/smartphone.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
 
 function Header() {
 
   const fitGeek = 'FitGeek'
-
-  
+  const styles = {
+    fontfamily: 'Playfair Display, serif',
+    fontSize: '2.5rem'
+  }
 
   return (
+    
     <>
-      <div className='upper_header'>
-          <div className='brand_name'> {/*This is for the logo_name */}
-              <h1>{fitGeek}</h1>
+      <div className='header'>
+          <div className='upper_header'>
+              <div className='brand_name'> {/*This is for the logo_name */}
+                  <h1>{fitGeek}</h1>
+              </div>
+              
+              <div className='user_verification'> {/*login and signup*/}
+                  <div className='log_in'>
+                      <button className='btn_log_in'>
+                        <h3>Login</h3>
+                      </button>
+                  </div>
+                  <div className='sign_up'>
+                      <button className="btn_sign_up">
+                        <h3>Sign Up</h3>
+                      </button>
+                  </div>
+              </div>
           </div>
+
           
-          <div className='user_verification'> {/*login and signup*/}
-              <div className='log_in'>
-                  <button className='btn_log_in'>
-                    <h3>Login</h3>
-                  </button>
+
+          <section className='container_1'>
+              <div className='typography'>
+                  <div className='mini_typography'> 
+                      <h4>#1 nutrition and diet planning App</h4>
+                  </div>
+
+                  <div className='main_typography'> 
+                      <h4>Reach your Goals</h4>
+                      <h6 style={styles}>with Fit-Geek</h6>
+                  </div>
+
+                  <div className='lower_typography'> 
+                      <h5>Build healthy habits with the all-in-one food, exercise, and calorie tracker</h5>
+                  </div>
+
+                  <div className='btn_section'> 
+                      <button>Start Today  <FontAwesomeIcon icon={faArrowRight} /> </button>
+                      
+                  </div>
               </div>
-              <div className='sign_up'>
-                  <button className="btn_sign_up">
-                    <h3>Sign Up</h3>
-                  </button>
-              </div>
-          </div>
+              
+              <div className='svg_img'>
+                  <img src={myImage} alt='not available rn'/>
+              </div> 
+
+          </section>
       </div>
 
     </>
