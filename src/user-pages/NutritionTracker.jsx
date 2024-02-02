8cd9bc8fce16b57cfd601fdfb2c4e4abf2c705e2
Akshay Kalushe <akshaykalushe35@gmail.com>
2024-02-02 19:16:22 +0530

    added the nutrition tracking pages

--- src/user-pages/NutritionTracker.jsx
+++ src/user-pages/NutritionTracker.jsx
@@ -1,43 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import "../user-pages/nutritionTrackerStyling.css";
+import { map } from 'zod';
 
 const NutritionTracker = () => {
     console.log("Component rendered");
     const [apiData, setApiData] = useState(null);
+    const [search, setSearch] = useState("");
+    const [getFoodInfo, setgetFoodInfo] = useState("");
 
-  useEffect(() => {
-    const fetchNutritionixAPI = async () => {
-      try {
-        const response = await fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-app-id': '240b598c',
-            'x-app-key': '246464e848f6edcfd6c3ea1578876791'
-          },
-          body: JSON.stringify({
-            "query": "grape"
-          })
-        });
+    const searchedQuery = (e) => {
+        let newValue = e.target.value;
+        setSearch(newValue);
+    }
 
-        if (response.ok) {
-          const data = await response.json();
-          setApiData(data);
-          console.log(apiData);
-        } else {
-          console.log("Error from fetching API data");
-        }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('clicked');
+        const apiKey = 'l282OtJUfRyHbkAyMwJG16W80hSPhfw6cUodPCuc';
+
+        try {
+          const response = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${apiKey}&query=${search}`);
+          if (response.ok) {
+              const data = await response.json();
+              console.log(data);
+              setApiData(data);
+              // console.log(data.foods);
+              let trackedFood = data.foods;
+              setgetFoodInfo(trackedFood)
+              console.log(getFoodInfo);
+          } else {
+              console.log("Error from fetching API data");
+              setgetFoodInfo([]);
+          }
       } catch (error) {
-        console.log(`Error in API: ${error.message}`);
+          console.log(`Error in API: ${error.message}`);
       }
-    };
+  };
 
-    fetchNutritionixAPI(); // eslint-disable-next-line
-  }, []); 
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="search">Search Here</label>
+                <input type="text" placeholder='Search here' name='search' value={search} onChange={searchedQuery} />
+                <button type="submit">Submit</button>
+            </form>
 
-  return (
-    <h1>hello</h1>
-  );
+            <section>
+            {getFoodInfo.map((item) => (
+                <li key={item.fdcId}>
+                  {item.description}: {item.dataType}
+                </li>
+              ))}
+            </section>
+        </>
+    );
 };
 
 export default NutritionTracker;
